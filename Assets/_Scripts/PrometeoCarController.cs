da481909e6b0f31ce4456660ff41a812932d6b11
slim_link <gamreromanchik87@mail.ru>
2023-10-30 21:56:05 +0500

    изменил скрипт прометея и коэффициенты трения

--- Assets/_Scripts/PrometeoCarController.cs
+++ Assets/_Scripts/PrometeoCarController.cs
@@ -22,25 +22,25 @@ public class PrometeoCarController : MonoBehaviour
       [Space(20)]
       //[Header("CAR SETUP")]
       [Space(10)]
-      [Range(20, 190)]
+      [Range(20, 500)]
       public int maxSpeed = 90; //The maximum speed that the car can reach in km/h.
-      [Range(10, 120)]
+      [Range(10, 300)]
       public int maxReverseSpeed = 45; //The maximum speed that the car can reach while going on reverse in km/h.
-      [Range(1, 10)]
+      [Range(1, 100)]
       public int accelerationMultiplier = 2; // How fast the car can accelerate. 1 is a slow acceleration and 10 is the fastest.
     [Space(10)]
-    [Range (1, 10)]
+    [Range (1, 100)]
     public int boost = 10;
     [Range(10, 45)]
     public int maxSteeringAngle = 27; // The maximum angle that the tires can reach while rotating the steering wheel.
-      [Range(0.1f, 1f)]
+      [Range(0.1f, 10f)]
       public float steeringSpeed = 0.5f; // How fast the steering wheel turns.
       [Space(10)]
-      [Range(100, 600)]
+      [Range(100, 1200)]
       public int brakeForce = 350; // The strength of the wheel brakes.      
-      [Range(1, 10)]
+      [Range(1, 100)]
       public int decelerationMultiplier = 2; // How fast the car decelerates when the user is not using the throttle.
-      [Range(1, 10)]
+      [Range(1, 100)]
       public int handbrakeDriftMultiplier = 5; // How much grip the car loses when the user hit the handbrake.
       [Space(10)]
       public Vector3 bodyMassCenter; // This is a vector that contains the center of mass of the car. I recommend to set this value
