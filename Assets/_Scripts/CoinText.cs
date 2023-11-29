2ea405217c3644a1e40e1acce4ca435ab0300335
deathlydh <70377878+deathlydh@users.noreply.github.com>
2023-11-30 04:03:51 +0500

    add: models scripts
    Я уже даже не помню что там как там что добавил что убрал

--- Assets/_Scripts/CoinText.cs
+++ Assets/_Scripts/CoinText.cs
@@ -1,18 +1,55 @@
 using UnityEngine;
+using YG;
 using UnityEngine.UI;
 
 public class CoinText : MonoBehaviour
 {
     Text CoinTexts;
+    private int currentCoins = 0; // переменная для хранения текущего количества монет
+
+    [SerializeField] private Button FirstButtonReward;
 
     void Start()
     {
         CoinTexts = GetComponent<Text>();
-        UpdateCoins(PlayerPrefs.GetInt("SavedPoints", 0)); // Обновление количества монет в главном меню
+        currentCoins = PlayerPrefs.GetInt("SavedPoints", 0); // Получаем сохраненное количество монет
+        UpdateCoins(currentCoins); // Обновляем текст с количеством монет
+        YandexGame.FullscreenShow();
+        FirstButtonReward.onClick.AddListener(delegate { ExampleOpenRewardAd(1); });
     }
 
     public void UpdateCoins(int coins)
     {
         CoinTexts.text = coins.ToString();
+    }
+
+    void Rewarded(int id)
+    {
+        if (id == 1)
+        {
+            AddMoney(1000);
+        }
+    }
+
+    private void AddMoney(int coins)
+    {
+        currentCoins += coins; // увеличиваем текущее количество монет на значение coins
+        PlayerPrefs.SetInt("SavedPoints", currentCoins); // Сохраняем значение монет в PlayerPrefs
+        UpdateCoins(currentCoins); // Обновляем текст с количеством монет
+    }
+
+    void ExampleOpenRewardAd(int id)
+    {
+        YandexGame.RewVideoShow(id);
+    }
+
+    private void OnEnable()
+    {
+        YandexGame.RewardVideoEvent += Rewarded;
+    }
+
+    private void OnDisable()
+    {
+        YandexGame.RewardVideoEvent -= Rewarded;
     }
 }
--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using YG;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
